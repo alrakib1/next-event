ec8038041c19ffaa84dff146a1bd79eb6a4a6772
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2024-03-04 21:53:20 +0600

    added condition for error of filtered events

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -5,8 +5,37 @@ import React from "react";
 const FilteredEventsPage = () => {
   const router = useRouter();
 
-getFilteredEvents()
+  const filteredData = router.query.slug;
+  console.log(filteredData);
 
+  if (!filteredData) {
+    return <p className="center">Loading...</p>;
+  }
+
+  const filteredYear = filteredData[0];
+  const filteredMonth = filteredData[1];
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return <p>Invalid filter. Please adjust your values!</p>;
+  }
+
+  const filteredEvents = getFilteredEvents({
+    year: numYear,
+    month: numMonth,
+  });
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return <p>No events found for the chosen filter!</p>;
+  }
 
   return (
     <div>
