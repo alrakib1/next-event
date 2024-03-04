5ccc225ef28e967597fec555e1f948db8a6b8e85
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2024-03-04 21:08:57 +0600

    header completed

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import EventList from "@/components/events/event-list";
+import { getAllEvents } from "@/dummy-data";
+import React from "react";
 
 const AllEventsPage = () => {
-    return (
-        <div>
-            all events will be shown here
-        </div>
-    );
+  const events = getAllEvents();
+  return (
+    <>
+      <h1 className="text-center text-4xl font-semibold">All Events</h1>
+      <EventList items={events} />
+    </>
+  );
 };
 
 export default AllEventsPage;
--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,7 @@ export default function Home() {
       <Head>
         <title>Next Event</title>
       </Head>
+      
       <EventList items={featuredEvents} />
     </>
   );
